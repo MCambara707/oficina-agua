0437a443ec7448a99873a79f3ce7d21cd9a7414a
MCambara <mcambaraa@miumg.edu.gt>
2026-08-24 21:52:16 -0600

    AdminLTE 4 integration + session adjustment/file caching

---
+++ resources/views/admin-demo.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Demo')
+
+@section('content_header')
+    <h1>Panel de prueba</h1>
+@stop
+
+@section('content')
+    <p>Si ves el sidebar y el navbar, AdminLTE ya está funcionando de verdad.</p>
+@stop
