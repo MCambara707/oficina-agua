89cb12618deaddef8b85fa17cd0952e10347709d
Mauricio9182 <pablitocarrillo9182@gmail.com>
2026-08-28 01:24:40 -0600

    feat: CRUD completo de tarifas (AQ-11)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\TarifaController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -10,4 +11,6 @@ Route::get('/admin-demo', function () {
     return view('admin-demo');
 });
 
+
 Route::resource('clientes', ClienteController::class)->except('show');
+Route::resource('tarifas', TarifaController::class)->except('show');
